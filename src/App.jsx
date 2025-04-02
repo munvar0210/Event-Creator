c6b272a2468618939c94bdfc72181541a35fbf31
munvar0210 <munvar@aptitudeguruhem.com>
2025-04-02 12:49:21 +0530

    registered list is completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import EventsPage from "./pages/Event/event";
 import WorkshopRegistration from "./pages/EventDetails/eventDetails";
 import MultiStepForm from "./pages/Form/multiStepForm";
 import EventManagementPage from "./pages/EventManagement/eventManagement";
+import RegisteredMembers from "./pages/RegisteredMembers/registeredMembers";
 import "./index.css";
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/event/:id" element={<WorkshopRegistration />} />
         <Route path="/form" element={<MultiStepForm />} />
         <Route path="/edit" element={<EventManagementPage />} />
+        <Route path="/registered-members" element={<RegisteredMembers />} />
       </Routes>
     </Router>
   );
---
+++ src/data/registeredMembersData.js
@@ -0,0 +1,62 @@
+export const membersData = [
+  {
+    name: "James",
+    email: "james123455@gmail.com",
+    phone: "+91 1234567890",
+    avatar: "https://randomuser.me/api/portraits/men/32.jpg",
+  },
+  {
+    name: "Liam",
+    email: "liam@gmail.com",
+    phone: "+91 1234567890",
+    avatar: "https://randomuser.me/api/portraits/men/67.jpg",
+  },
+  {
+    name: "Aadhira",
+    email: "aadhira@gmail.com",
+    phone: "+91 1234567890",
+    avatar: "https://randomuser.me/api/portraits/women/44.jpg",
+  },
+  {
+    name: "Charlotte",
+    email: "charlotte234@gmail.com",
+    phone: "+91 1234567890",
+    avatar: "https://randomuser.me/api/portraits/women/33.jpg",
+  },
+  {
+    name: "Oliver",
+    email: "oliver9876@gmail.com",
+    phone: "+91 1234567890",
+    avatar: "https://randomuser.me/api/portraits/men/22.jpg",
+  },
+  {
+    name: "Alden",
+    email: "alden678590@gmail.com",
+    phone: "+91 1234567890",
+    avatar: "https://randomuser.me/api/portraits/men/90.jpg",
+  },
+  {
+    name: "James Aurther",
+    email: "jamesaurther987@gmail.com",
+    phone: "+91 1234567890",
+    avatar: "https://randomuser.me/api/portraits/men/41.jpg",
+  },
+  {
+    name: "Amelia James",
+    email: "ameliajames201256@gmail.com",
+    phone: "+91 1234567890",
+    avatar: "https://randomuser.me/api/portraits/women/50.jpg",
+  },
+  {
+    name: "Aurelia",
+    email: "aurelia7865@gmail.com",
+    phone: "+91 1234567890",
+    avatar: "https://randomuser.me/api/portraits/women/63.jpg",
+  },
+  {
+    name: "Priscilla",
+    email: "priscilla768934@gmail.com",
+    phone: "+91 1234567890",
+    avatar: "https://randomuser.me/api/portraits/women/79.jpg",
+  },
+];
